749252f6e795114e77069f7a41ba7210e4963aa5
鷲見公崇 <sumi@emol.jp>
2023-01-08 21:57:01 +0900

    github workflowsの修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS,
   trailingSlash: true,
   assetPrefix: '/',
   images: {
