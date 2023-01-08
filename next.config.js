596a5f9b50509ee06b15f92165038d4ce3784e0c
鷲見公崇 <sumi@emol.jp>
2023-01-08 21:17:47 +0900

    next.config.jsの修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "",
+  trailingSlash: true,
+  assetPrefix: '/',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 module.exports = nextConfig
